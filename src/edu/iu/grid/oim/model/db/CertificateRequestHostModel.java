f4a6aa5c2508d564a1a8c1dbfaf48ff515646e6f
mvkrenz <mvkrenz@iu.edu>
2016-08-18 14:31:15 -0400

    undone changes

--- src/edu/iu/grid/oim/model/db/CertificateRequestHostModel.java
+++ src/edu/iu/grid/oim/model/db/CertificateRequestHostModel.java
@@ -966,14 +966,14 @@ public class CertificateRequestHostModel extends CertificateRequestModelBase<Cer
 		//CertificateManager cm = CertificateManager.Factory(context, rec.approver_vo_id);
 
 
-	//	String issuer_dn = ""; //by default
+		String issuer_dn = ""; //by default
 		ArrayList<Certificate> chain = null;
 		try {
 			if (rec.getPKCS7s()[0] != null) {
 				chain = CertificateManager.parsePKCS7(rec.getPKCS7s()[0]);
 				X509Certificate c0 = CertificateManager.getIssuedX509Cert(chain);
 				X500Principal issuer = c0.getIssuerX500Principal();
-				String issuer_dn = CertificateManager.X500Principal_to_ApacheDN(issuer);
+				issuer_dn = CertificateManager.X500Principal_to_ApacheDN(issuer);
 				log.debug("issuer dn is " + issuer_dn);
 			}
 		} catch (CertificateException e2) {
